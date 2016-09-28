8be26cdb05bf9b99c3d3302eca6dbdd6988dd940
Bastian Koppelmann <kbastian@mail.uni-paderborn.de>
2016-09-28 15:26:22 +0200

    Remove unused files.
    Signed-off-by: Bastian Koppelmann <kbastian@mail.uni-paderborn.de>

--- pull-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-repos=$(find . -iname ".git" | rev | cut -c 5- | rev)
-base_dir=$(pwd)
-for repo in $repos; do
-    echo "Pulling $repo"
-    cd $repo
-    git pull
-    cd $base_dir
-done
